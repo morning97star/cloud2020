9df810e00da48f2c617ab6c979c00e25ac2158ff
morningstar <1462812201@qq.com>
2020-12-03 15:13:00 +0800

    mybatis自动生成utils

--- cloud-consumer-9001/src/main/java/com/zcx/cloud/consumer/controller/OrderController.java
+++ cloud-consumer-9001/src/main/java/com/zcx/cloud/consumer/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.zcx.cloud.consumer.controller;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,14 @@ public class OrderController {
     @GetMapping("consumer/payment/get")
     public String get() {
         return restTemplate.getForObject(PAYMENT_SERVICE_URL + "payment/get", String.class);
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("r1__> " + StringUtils.equals("", null));
+        System.out.println("r2__> " + StringUtils.equals("", ""));
+        System.out.println("r3__> " + StringUtils.equals("", "   "));
     }
 
 }
